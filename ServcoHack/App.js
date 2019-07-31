4f0adc70aff4314cf908cd410add80b01e12ec82
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2019-07-31 10:45:55 -1000

    hi guys

--- ServcoHack/App.js
+++ ServcoHack/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import jah from "./components/test";
+import Jah from "./components/test";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Jah />
     </View>
   );
 }
@@ -12,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ ServcoHack/components/test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Jah = props => {
+  return <Text title="jah">HELLO FUCK YOU</Text>;
+};
+
+export default Jah;

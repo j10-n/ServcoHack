0855180418f340c106052ad943a4f25b0b255c93
justen <justen@justens-MacBook-Pro.local>
2019-07-31 17:37:00 -1000

    added clickable event for clicking on the car

--- ServcoHack/components/Home.js
+++ ServcoHack/components/Home.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Header, Button, Card, Icon, Image, Text } from "react-native-elements";
-import { StyleSheet, View, TouchableHighlight } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import { ScrollView } from "react-native-gesture-handler";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class Home extends Component {
   constructor(props) {
@@ -23,62 +23,49 @@ export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView>
-          <Text style={styles.text} h3>
-            SERVCO OHANA
+        <Text style={styles.text} h3>
+          SERVCO OHANA
+        </Text>
+        <Text title="welcomeuser">Welcome Jen!</Text>
+
+        <Card title="2019 Toyota PRIUS XLE HYBRID">
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.navigate("CarbonData");
+            }}
+          >
+            <Image
+              source={{
+                uri:
+                  "https://c4d709dd302a2586107d-f8305d22c3db1fdd6f8607b49e47a10c.ssl.cf1.rackcdn.com/thumbnails/stock-images/8373897860e6a795bb1879dc16c761ea.png"
+              }}
+              style={{ width: 330, height: 230, resizeMode: "contain" }}
+            />
+          </TouchableOpacity>
+          <Text style={{ marginBottom: 10 }}>
+            Click on your car to view more details...
           </Text>
-          <Text title="welcomeuser">Welcome Jen!</Text>
+          <Text>Warranty Expiration Date:</Text>
+          <Text>Maintenance Due Date:</Text>
 
-          <Card title="2019 Toyota PRIUS XLE HYBRID">
-            <TouchableHighlight
-              onPress={() => {
-                this.props.navigation.navigate("CarbonData");
-              }}
-            >
-              <Image
-                source={{
-                  uri:
-                    "https://c4d709dd302a2586107d-f8305d22c3db1fdd6f8607b49e47a10c.ssl.cf1.rackcdn.com/thumbnails/stock-images/8373897860e6a795bb1879dc16c761ea.png"
-                }}
-                style={{ width: 330, height: 230, resizeMode: "contain" }}
-              />
-            </TouchableHighlight>
-            <Text style={{ marginBottom: 10 }}>
-              Click on your car to view more details...
-            </Text>
-            <Text>Warranty Expiration Date:</Text>
-            <Text>Maintenance Due Date:</Text>
-            <Button
-              style={styles.button}
-              type="solid"
-              buttonStyle={{
-                backgroundColor: "#18228c"
-              }}
-              title="Schedule"
-              onPress={() => {
-                this.props.navigation.navigate("Schedule_Calendar");
-              }}
-            />
-            <Button
-              type="solid"
-              disabled={true}
-              style={{ marginTop: 10 }}
-              title="Car Payment"
-            />
-
-            <Button
-              type="solid"
-              style={{ marginTop: 40 }}
-              title="Compare Carbon Output Here"
-              buttonStyle={{
-                backgroundColor: "#18228c"
-              }}
-              onPress={() => {
-                this.props.navigation.navigate("ChooseCar");
-              }}
-            />
-          </Card>
-        </ScrollView>
+          <Button
+            style={styles.button}
+            type="solid"
+            buttonStyle={{
+              backgroundColor: "#18228c"
+            }}
+            title="Schedule"
+            onPress={() => {
+              this.props.navigation.navigate("Schedule_Calendar");
+            }}
+          />
+          <Button
+            type="solid"
+            disabled={true}
+            style={{ marginTop: 10 }}
+            title="Car Payment"
+          />
+        </Card>
       </View>
     );
   }
@@ -90,8 +77,7 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   text: {
-    margin: 10,
-    justifyContent: "center"
+    margin: 10
   },
   button: {
     marginTop: 40
